4ab42d35d68d3b5588e871c57f75bb1a3b5f42ea
yueuni <eun@kakao.com>
2022-08-26 20:03:14 +0900

    [INIT] 0.3 FE Setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require("express")
-
 const app = express()
 
-console.log("start")
+const port = 3000
 
-app.listen(3000)
+app.set("view engine", "pug")
+app.set("views", __dirname+"/views")
+app.use("/public", express.static(__dirname+"/public"))
+app.get("/", (req, res) => res.render("home"))
+app.get("/*", (req, res) => res.redirect("/"))
+
+const handleListen = () => console.log(`Listening on http://localhost:${port}/`)
+app.listen(port, handleListen)
