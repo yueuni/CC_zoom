62a2c1ffe5a16592b083520190cb194dbc35816c
yueuni <eun@kakao.com>
2022-08-27 15:39:56 +0900

    [FEAT] 1.5 Recap Websocket 연결

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,9 @@
 const socket = new WebSocket(`ws://${window.location.host}`)
+
+const onOpen = _ => console.log("Connected to Server")
+const onMsg = msg => console.log(`From server : ${msg.data}`)
+const onClose = _ => console.log("Disconnected from server")
+
+socket.addEventListener("open", _ => onOpen)
+socket.addEventListener("message", onMsg)
+socket.addEventListener("close", onClose)
--- src/server.js
+++ src/server.js
@@ -11,11 +11,17 @@ app.use("/public", express.static(__dirname+"/public"))
 app.get("/", (_, res) => res.render("home"))
 app.get("/*", (_, res) => res.redirect("/"))
 
-const handleListen = () => console.log(`Listening on http://localhost:${port}/`)
 
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
-wss.on("connection", (socket) => console.log(socket))
+const onClose = _ => console.log("Disconnected from the Browser")
+const onMsg = msg => console.log(msg)
 
-app.listen(port, handleListen)
+wss.on("connection", socket => {
+    console.log("Connected to Browser")
+    socket.on("close", onClose)
+    socket.on("message", onMsg)
+})
+
+server.listen(port, _ => console.log(`Listening on http://localhost:${port}/`))
