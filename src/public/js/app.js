8cb788330ed1c816fd908cfd157b34b7d73fa022
yueuni <eun@kakao.com>
2022-08-27 16:53:21 +0900

    1.9 ws를 이용한 간단 채팅 구현  - 닉네임 설정  - 나 이외의 브라우저에게 메세지 전송 (강의에선 "나를 포함한"으로 진행됨)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,9 +1,36 @@
+const msgList = document.querySelector("ul")
+const nickForm = document.querySelector("#nick")
+const msgForm = document.querySelector("#msg")
+
 const socket = new WebSocket(`ws://${window.location.host}`)
 
 const onOpen = _ => console.log("Connected to Server")
-const onMsg = msg => console.log(`From server : ${msg.data}`)
+const onMsg = msg => {
+    const li = document.createElement('li')
+    li.innerText = msg.data || msg
+    msgList.append(li)
+}
 const onClose = _ => console.log("Disconnected from server")
 
 socket.addEventListener("open", _ => onOpen)
 socket.addEventListener("message", onMsg)
 socket.addEventListener("close", onClose)
+
+const sendToServer = (socket, type, payload) => socket.send(JSON.stringify({type, payload}))
+
+// 메세지 전송
+msgForm.addEventListener("submit", event => {
+    event.preventDefault()
+    const input = msgForm.querySelector('input')
+    sendToServer(socket, "new_message", input.value)
+    onMsg(`You : ${input.value}`)
+    input.value = ""
+})
+
+// 닉네임 저장
+nickForm.addEventListener("submit", event => {
+    event.preventDefault()
+    const input = nickForm.querySelector('input')
+    sendToServer(socket, "nickname", input.value)
+    input.value = ""
+})
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const WebSocket = require('ws')
 const express = require("express")
+const { parse } = require('path')
 const app = express()
 
 const port = 3000
@@ -16,12 +17,29 @@ const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
 const onClose = _ => console.log("Disconnected from the Browser")
-const onMsg = msg => console.log(msg)
+const onMsg = msg => console.log(msg.toString())
+
+const sockets = []
 
 wss.on("connection", socket => {
+    socket["nickname"] = "Anon"
+    sockets.push(socket)
     console.log("Connected to Browser")
+
     socket.on("close", onClose)
-    socket.on("message", onMsg)
+    socket.on("message", msg => {
+        const message = JSON.parse(msg)
+
+        switch(message.type) {
+            case "new_message": 
+                sockets.forEach(s => {
+                    if(s != socket) s.send(`${socket.nickname} : ${message.payload}`)
+                })
+                break
+            case "nickname": 
+                socket["nickname"] = message.payload
+        }
+    })
 })
 
 server.listen(port, _ => console.log(`Listening on http://localhost:${port}/`))
