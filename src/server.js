85bebebb1b5369e6d0267b8c295fe746296f30c7
yueuni <eun@kakao.com>
2022-09-03 16:49:55 +0900

    [RESET] 프로젝트 재셋팅 (채팅 -> 비디오스트리밍)

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const wsServer = new Server(httpServer, {
     }
 })
 
-instrumentj(wsServer, {
+instrument(wsServer, {
     auth: false
 })
 
@@ -41,44 +41,7 @@ const getPublicRooms = _ => {
 }
 
 wsServer.on("connection", socket => {
-    console.log(getPublicRooms())
-    socket.to(socket.id).emit("room_changed", getPublicRooms())
-
-    // 방 참여
-    socket.on("enter_room", (roomName, nickname, done) => {
-        socket['nickname'] = nickname
-        socket.join(roomName)
-        socket.to(roomName).emit("joined_member", nickname)
-        done()
-        wsServer.sockets.emit("room_changed", getPublicRooms())
-    })
-
-    // 닉네임 변경
-    socket.on("save_name", (nickname, done) => {
-        const before = socket['nickname']
-        socket['nickname'] = nickname
-        const joinedRooms = getJoinedRooms(socket.id)
-        socket.to(joinedRooms).emit("send_noti", `[${before}]님의 닉네임이 [${nickname}]으로 변경되었습니다`)
-        socket.emit("send_noti", `[${before}]님의 닉네임이 [${nickname}]으로 변경되었습니다`)
-        done()
-    })
-
-    // 연결 끊김
-    socket.on("disconnecting", _ => {
-        const joinedRooms = getJoinedRooms(socket.id)
-        socket.to(joinedRooms).emit("disconnect_member", socket['nickname'])
-        wsServer.sockets.emit("room_changed", getPublicRooms())
-    })
-    socket.on("disconnect", _ => {
-        wsServer.sockets.emit("room_changed", getPublicRooms())
-    })
-
-    // 새 메세지
-    socket.on("new_message", (msg, done) => {
-        const joinedRooms = getJoinedRooms(socket.id)
-        socket.to(joinedRooms).emit('new_message', `${socket.nickname}: ${msg}`)
-        done()
-    })
+    
 })
 
 httpServer.listen(port, _ => console.log(`Listening on http://localhost:${port}/`))
